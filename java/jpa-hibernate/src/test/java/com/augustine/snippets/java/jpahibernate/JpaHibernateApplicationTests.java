a3f34ce0c241eb15aef29ab0f885acce80784ef8
Uncle.Phuong <augustine.nguyen@theaccessgroup.com>
2024-05-02 17:31:25 +0700

    Init jpa-hibernate snippet

---
+++ java/jpa-hibernate/src/test/java/com/augustine/snippets/java/jpahibernate/JpaHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.augustine.snippets.java.jpahibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
